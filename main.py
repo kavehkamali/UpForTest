f0687a8216d4300b95463fa2a16414444b55648c
kavehkamali <kaveh.kamali@gmail.com>
2023-12-21 13:19:37 -0500

    change app.py to main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from pathlib import Path
 import logging
 
 app = FastAPI()
-client = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
+client = OpenAI(api_key="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")
 
 # logging
 log = logging.getLogger("uvicorn")
@@ -37,52 +37,7 @@ async def index(request: Request):
         log.exception("An HTTPException occurred: %s", exc.detail)
         return http_exception_handler(exc)
 
-# This was the route with old completions API
-# @app.post("/ask_gpt4/")
-# async def ask_gpt4(query_params: QueryModel, model: Optional[str] = "text-davinci-003") -> dict:
-#     """
-#     Post Route receive a query and return a response with OpenAIAPI for chat GPT
-#     :param query_params: User input in the form of questions to chat GPT
-#     :param model: type of model as per OpenAIAPI specifications.
-#     :return: Json in the form of a JSONResponse FastAPi instance
-#     """
-#     try:
-#         # Call the OpenAI API
-#         # The model used is not gpt4 to use gpt4 as a model a different API has to be used and
-#         # in specific the openai.ChatCompletion.create(.....)
-#         response = openai.Completion.create(
-#             engine=model,
-#             prompt=query_params.user_input,
-#             max_tokens=1000,
-#             n=1,
-#             stop=None,
-#             temperature=0.5,
-#         )
-#
-#         if len(response.choices) > 0 and hasattr(response.choices[0], "text"):
-#             answer = response.choices[0].text.strip()
-#             return {"response": answer}
-#         else:
-#             error_msg = "ChatGPT response does not contain text attribute."
-#             log.error(error_msg)
-#             raise GPTException(error_msg)
-#
-#             # return {"error": "ChatGPT response does not contain text attribute."}
-#     except GPTException as e:
-#         raise e
-#     except Exception as e:
-#         log.error(f"Exception occurred: {str(e)}")
-#         if not query_params.user_input:  # Empty user_input case
-#             raise GPTException("Empty user_input", status_code=400)
-#         else:
-#             raise GPTException(str(e))
-#     # except Exception as e:
-#     #     return {"error": str(e)}
-#
-#     # Exception handling
-
-
-@app.post("/ask_gpt4/")
+@app.post("/UpForTest/")
 async def ask_gpt4(query_params: QueryModel) -> JSONResponse:
     """
     Endpoint to receive a query and return a response using OpenAI's Chat Completions API
